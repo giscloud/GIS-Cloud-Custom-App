2f1a067e094efb705d47555efa0727eb8433ef12
Marko <marko@giscloud.com>
2013-03-29 14:58:17 +0100

    removed few layer list glitches

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,32 +8,25 @@ jQuery(function(){
     $(window).resize(body_resize);
     body_resize();
 
-
-
     gcapp.addEvent("ready", function(){
 
         gcapp.gcmap.addEvent("ready", function(){
-                    console.log("das");
+
             var MyControl = L.Control.extend({
                 options: {
                     position: 'topright'
                 },
 
                 onAdd: function (map) {
-                    // create the control container with a particular class name
                     var container = L.DomUtil.create('div', 'leaflet-control-gc-layers');
 
                     el = $(container);
                     el.addClass("gc-pointer");
                     el.html('<div style="margin:0;" class="gc-icon gc-layers" ></div>');
 
-                    el.click(function(){
-                        console.log(232);
+                    el.click(function(e){
                         $('#gc_layers_box').toggle();
                     });
-
-                    console.log(el, container);
-                    // ... initialize other DOM elements, add listeners, etc.
 
                     return container;
                 }
