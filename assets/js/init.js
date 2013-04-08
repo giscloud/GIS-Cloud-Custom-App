a6a46b583e999a753302c9dfe1080a20fdd68b7d
Marko <marko@giscloud.com>
2013-04-08 21:31:00 +0200

    added github fork me button

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,20 +1,19 @@
 jQuery(function(){
 
+    // Resize event logic
     function body_resize(){
         var body_height = $("body").height();
         $("#mapPanel").height(body_height-280);
     }
-
     $(window).resize(body_resize);
     body_resize();
 
     gcapp.addEvent("ready", function(){
-
         gcapp.gcmap.addEvent("ready", function(){
-
+            // Add custom Leaflet control, icon for toggle the layer list module
             var MyControl = L.Control.extend({
                 options: {
-                    position: 'topright'
+                    position: 'topleft'
                 },
 
                 onAdd: function (map) {
@@ -22,7 +21,7 @@ jQuery(function(){
 
                     el = $(container);
                     el.addClass("gc-pointer");
-                    el.html('<div style="margin:0;" class="gc-icon gc-layers" ></div>');
+                    el.html('<div class="gc-icon gc-layers" ></div>');
 
                     el.click(function(e){
                         $('#gc_layers_box').toggle();
