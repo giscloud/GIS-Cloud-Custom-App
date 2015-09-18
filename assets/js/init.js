8a20ff64e38452dc1fcc3a4200b76594ca83298c
marko <mapolovina>
2015-09-18 17:56:28 +0200

    Changes on demo application - included new layer list and datagrid.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,38 +1,41 @@
-jQuery(function(){
-
+jQuery(function() {
+    
+    giscloud.ready(function() {
+        
+        giscloud.ui.ready.then(function() {
+            var activeLayer = 1156;
+            var orderBy = {
+                    orderByLayerId: activeLayer,
+                    orderByExpression: "ogc_fid:desc"
+            };
+            var layerList = new giscloud.ui.LayerList(giscloud.ui.map, 'gc_layers_box', true);
+            var datagrid = new giscloud.ui.FeatureDatagrid('#featureDatagrid', {
+                viewer: giscloud.ui.map,
+                activeLayerId: activeLayer,
+                datagridOpts:{
+                    "ordering": true  
+                },
+                orderBy: orderBy
+            });
+            
+            layerList.bind("changed", function (event, object, id, layer) {
+                var newLayerId = parseInt(object[0].id, 10);
+                datagrid.setActiveLayerId(newLayerId);
+            });  
+        });
+        
+        $('#sign_in_button').click(function () {
+            showLoginForm();
+        });
+    });
+    
     // Resize event logic
-    function body_resize(){
+    function body_resize() {
         var body_height = $("body").height();
         $("#mapPanel").height(body_height-280);
     }
+    
     $(window).resize(body_resize);
     body_resize();
-
-    gcapp.addEvent("ready", function(){
-        gcapp.gcmap.addEvent("ready", function(){
-            // Add custom Leaflet control, icon for toggle the layer list module
-            var MyControl = L.Control.extend({
-                options: {
-                    position: 'topleft'
-                },
-
-                onAdd: function (map) {
-                    var container = L.DomUtil.create('div', 'leaflet-control-gc-layers');
-
-                    el = $(container);
-                    el.addClass("gc-pointer");
-                    el.html('<div class="gc-icon gc-layers" ></div>');
-
-                    el.click(function(e){
-                        $('#gc_layers_box').toggle();
-                    });
-
-                    return container;
-                }
-            });
-
-            gcapp.leaflet.addControl(new MyControl());
-        });
-    });
 
 });
