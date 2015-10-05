912a73e14844cb99621a71b7a0050a7e6ed3ebcc
marko <mapolovina>
2015-10-05 17:20:37 +0200

    If search is selected - close layer list.

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,9 +8,10 @@ jQuery(function() {
                     orderByLayerId: activeLayer,
                     orderByExpression: "ogc_fid:desc"
             };
+            var viewer = giscloud.ui.map;
             var layerList = new giscloud.ui.LayerList(giscloud.ui.map, 'gc_layers_box', true);
-            var datagrid = new giscloud.ui.FeatureDatagrid('#featureDatagrid', {
-                viewer: giscloud.ui.map,
+            var datagrid = new giscloud.ui.FeatureDatagrid('#feature-datagrid', {
+                viewer: viewer,
                 activeLayerId: activeLayer,
                 datagridOpts:{
                     "ordering": true  
@@ -18,10 +19,17 @@ jQuery(function() {
                 orderBy: orderBy
             });
             
+            layerList.bind('ready', function () {
+                $('#search_toggle_bt').click(function () {
+                    $('#gc_layers_box').hide();
+                });
+            });
+            
             layerList.bind("changed", function (event, object, id, layer) {
                 var newLayerId = parseInt(object[0].id, 10);
                 datagrid.setActiveLayerId(newLayerId);
             });  
+            
         });
         
         $('#sign_in_button').click(function () {
